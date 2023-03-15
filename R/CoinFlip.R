e26896c25ccff4465a7f82c01e789dbf452940fd
jaeinlee923 <122325091+jaeinlee923@users.noreply.github.com>
2023-03-15 09:33:09 -0700

    Add files via upload

---
+++ R/CoinFlip.R
@@ -0,0 +1,18 @@
+#' CoinFlip Function
+#'
+#' This function flips a coin
+#' @return returns a character string
+#' @keywords coin flip
+#' @export
+#' @examples
+#' CoinFlip()
+#' 
+CoinFlip <- function(){
+  n <- sample(0:1, 1)
+  if(n==0){
+    print("You got heads")
+  }
+  else {
+    print("You got tails")
+  }
+}
